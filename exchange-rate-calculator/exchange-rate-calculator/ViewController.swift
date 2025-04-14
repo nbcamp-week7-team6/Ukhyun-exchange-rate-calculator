b17106383b11cf1a682e3179041b28866cfdaff5
UkhyunGo <dnrwk99@gmail.com>
2025-04-14 14:01:05 +0900

    chore: 프로젝트 생성

---
+++ exchange-rate-calculator/exchange-rate-calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  exchange-rate-calculator
+//
+//  Created by GO on 4/14/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+
+
+}
+
