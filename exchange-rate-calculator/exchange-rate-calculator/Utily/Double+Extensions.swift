f662114460b5d425eed53afce05941564725fcd5
UkhyunGo <dnrwk99@gmail.com>
2025-04-16 21:25:22 +0900

    fix: NumberFormatter 소수점 범위 수정

--- exchange-rate-calculator/exchange-rate-calculator/Utily/Double+Extensions.swift
+++ exchange-rate-calculator/exchange-rate-calculator/Utily/Double+Extensions.swift
@@ -8,6 +8,7 @@ extension Double {
         let formatter = NumberFormatter()
         formatter.numberStyle = .decimal
         formatter.maximumFractionDigits = 4
+        formatter.minimumFractionDigits = 4
         return formatter
     }()
 }
