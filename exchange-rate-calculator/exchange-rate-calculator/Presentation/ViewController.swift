bf03913f569b77bd9f9e074347310712b8a9c12d
UkhyunGo <dnrwk99@gmail.com>
2025-04-16 20:50:23 +0900

    feat: Lv 1 구현 완료

--- exchange-rate-calculator/exchange-rate-calculator/Presentation/ViewController.swift
+++ exchange-rate-calculator/exchange-rate-calculator/Presentation/ViewController.swift
@@ -4,6 +4,9 @@ import SnapKit
 class ViewController: UIViewController {
     private let tableView = UITableView()
     
+    // 튜플 형태로 저장
+    private var exchangeRates: [(currency: String, rate: Double)] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         loadData()
@@ -11,27 +14,25 @@ class ViewController: UIViewController {
     }
 }
 
-extension ViewController{
+extension ViewController {
     private func configureUI() {
-        setUI()
-        setHierarchy()
-        setTableView()
+        setUpViews()
+        setUpConstraints()
+        setUpTableView()
     }
     
-    private func setUI() {
+    private func setUpViews() {
         view.backgroundColor = .white
         [tableView].forEach { view.addSubview($0) }
     }
     
-    private func setHierarchy() {
+    private func setUpConstraints() {
         tableView.snp.makeConstraints { make in
             make.size.equalToSuperview()
         }
     }
     
-    private func setTableView() {
-        tableView.backgroundColor = .cyan
-        
+    private func setUpTableView() {
         tableView.delegate = self
         tableView.dataSource = self
         
@@ -39,23 +40,34 @@ extension ViewController{
             ExchangeRateTableViewCell.self,
             forCellReuseIdentifier: ExchangeRateTableViewCell.id)
     }
-    
+}
+
+extension ViewController {
     private func loadData() {
         NetworkManager.shared
             .callRequest(api: .usd) { (data: ExchangeRateData) in
-                print(data)
-                if let krwRate = data.rates["KRW"] {
-                    print("USD -> KRW : \(krwRate)")
+                var rates: [(currency: String, rate: Double)] = []
+                let targetCurrencies = ["KRW", "USD", "JPY"]
+                for currency in targetCurrencies {
+                    if let rate = data.rates[currency] {
+                        rates.append((currency, rate))
+                    }
+                }
+                self.exchangeRates = data.rates.map { (code, rate) in
+                    (currency: code, rate: rate)
+                }
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
                 }
             } failHandler: {
-                print("Fail")
+                self.showAlert(title: "Error", message: "환율 데이터를 불러올 수 없습니다.")
             }
     }
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return exchangeRates.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -65,6 +77,9 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         ) as? ExchangeRateTableViewCell else {
             return ExchangeRateTableViewCell()
         }
+        let item = exchangeRates[indexPath.row]
+        let countryName = countryMapping[item.currency] ?? "미지원 국가"
+        cell.cellData(currency: "\(item.currency) (\(countryName))", rate: item.rate.decimalFormatted)
         
         return cell
     } 
